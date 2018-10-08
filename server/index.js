d1bf0cdddf968b378dd7eeb750aa580994370cb7
Lucas Selani de Melo <lucasselani@inatel.br>
2018-10-08 14:44:17 -0300

    Initial docker-node-mysql-phpmyadmin project
    Signed-off-by: Lucas Selani de Melo <lucasselani@inatel.br>

---
+++ server/index.js
@@ -0,0 +1,82 @@
+// IMPORTS
+var restify = require('restify');
+var mysql = require('mysql');
+
+// CONFIGS
+var port = process.env.PORT || 5000;
+var server = restify.createServer({
+    name: 'Backend SD'
+});
+var con = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'sd'
+}
+
+// ENDPOINT FUNCTIONS
+function getList(req, res, next) {
+    res.setHeader('content-type', 'application-json');
+    res.charSet('UTF-8');
+
+    var connection = mysql.createConnection(con);
+    connection.connect();
+
+    var strQuery = `SELECT * FROM toddy`;
+
+    connect.query(strQuery, function(err, rows, fields) {
+        if(!err) {
+            res.json(rows);
+        } else {
+            res.json(err);
+        }
+    });
+
+    connection.end();
+    next();
+}
+
+function update(req, res, next) {
+
+}
+
+function insert(req, res, next) {
+    res.setHeader('content-type', 'application-json');
+    res.charSet('UTF-8');
+    var toddy = {
+        lote: req.body.lote,
+        conteudo: req.body.conteudo,
+        validade: req.body.validade
+    }
+
+    var connection = mysql.createConnection(con);
+    connection.connect();
+
+    var strQuery = `INSERT INTO toddy (lote, conteudo, validade) 
+        VALUES (${toddy.lote}, ${toddy.conteudo}, ${toddy.validade})`;
+
+    connect.query(strQuery, function(err, rows, fields) {
+        if(!err) {
+            res.json(rows);
+        } else {
+            res.json(err);
+        }
+    });
+
+    connection.end();
+    next();
+}
+
+function exclude(req, res, next) {
+
+}
+
+// SERVER
+server.use(restify.plugins.bodyParser());
+server.get('/crud/listar', getList);
+server.post('/crud/atualizar', update);
+server.post('/crud/inserir', insert);
+server.post('/crud/excluir', exclude);
+server.listen(port, function() {
+    console.log('%s server started', server.name);
+});
